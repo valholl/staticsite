a9c220508b1b17ba78ebc09640e49ef6671ae926
Elena ``of Valhalla'' Grandi <valhalla@trueelena.org>
2016-12-28 22:04:25 +0100

    Don't try to read the title from reST docinfo, use the first title.

--- staticsite/restructuredtext.py
+++ staticsite/restructuredtext.py
@@ -154,10 +154,6 @@ class ReSTPage(Page):
         if date is not None and not isinstance(date, datetime.datetime):
             self.meta["date"] = dateutil.parser.parse(date)
 
-        if not self.meta.get("title", ""):
-            self.meta['title'] = self.rst_parts['title']
-
-
     def check(self, checker):
         self.resenv.render(self)
 
